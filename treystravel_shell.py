a24fd6efde867ce6f74c2cc17dfa71b76ba45472
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-05 13:00:03 -0500

    set up a function with format braces to automatically insert the gas type, price, and gallons automatically

--- treystravel_shell.py
+++ treystravel_shell.py
@@ -1,16 +1,25 @@
 import treystravel_core
 
+def treys_travel(gas, price):
+    amount = input('Our {} gas is ${} per gallon. How many gallons would you like?\n'.format(gas, price))
+    print('You have purchased', amount, '{}. Your total will be $' + int(pay_after(gallons, gas_price)))
 def main():
-    '''
+    message = '''
     Welcome to TREY\'S TRAVEL GAS STATION!
 
-    We are pleased to be your provider for the best gas around.
+We are pleased to be your provider for the best gas around.
 
-    Please choose the type of gas you would like to use today:
-    87. Regular 
-    89. Mid-Grade
-    92. Premium
-    '''
+Please choose the type of gas\nyou would like to use today:\n
+    87. Regular ($2.07)
+    89. Mid-Grade ($2.17)
+    92. Premium ($2.29)
+    
+Push "Q" to quit.
+'''
+    gas = ''
+    while gas != "Q".upper():
+        gas = input(message)
+        if gas == '87':
 
 
 
