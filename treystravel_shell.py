5a7f7c4ea91c1382f7981fe7dca6d4861c3dabe4
Trey Shelton <tshelton@basecampcodingacademy.org>
2017-07-05 16:26:59 -0500

    shortend my print statements

--- treystravel_shell.py
+++ treystravel_shell.py
@@ -1,8 +1,6 @@
 import treystravel_core
 
-def treys_travel(gas, price):
-    amount = input('\nOur {} gas is ${} per gallon. How many gallons would you like?\n'.format(gas, price))
-    print('You have purchased', amount, 'gallons. Your total will be ${}'.format(treystravel_core.gas_price(gas, amount)))
+
 def main():
     message = '''
     Welcome to TREY\'S TRAVEL GAS STATION!
@@ -17,21 +15,16 @@ Please choose the type of gas\nyou would like to use today:\n
 Push "Q" to quit.
 '''
     gas = ''
-    total_amount = 0
     while gas != "Q".upper():
         gas = input(message)
-        if gas == '87':
-            treys_travel('Regular', 2.07)
-            break
-        elif gas == '89':
-            treys_travel('Mid-Grade', 2.17)
-            break
-        elif gas == '92':
-            treys_travel('Premium', 2.29)
+        if gas == '87' or gas == '89' or gas == '92':
+            print(treystravel_core.treys_travel(gas, treystravel_core.gas_price(gas))) 
             break
         elif gas == 'Q':
-            total_amount += treystravel_core.gas_price(gas, amount)
-            print('Your total cost is $' + str(total_amount))
+            print('Program Abandoned.')
+            break
+    
+         
 
 
 if __name__ == '__main__':
